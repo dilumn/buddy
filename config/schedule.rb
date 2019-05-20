cdb8499ecee41e6ca0069820f33162fef11d34a6
dilumn <dilumnavanjana@gmail.com>
2019-05-20 11:59:47 +0800

    change scheduler to run daily

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,5 +3,6 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.every '1d' do
-  p ReminderHelper.today_reminders
+  # Slack notification should come here
+  ReminderHelper.today_reminders
 end
